aa4d00e951791c3ac7b00c41f42f895deb4b3cdc
JerryLee <leechiyin1990@gmail.com>
2019-09-10 17:38:48 +0930

    indicator

--- js/custom.js
+++ js/custom.js
@@ -6,6 +6,9 @@ var gs5=document.getElementById("gs5");
 var gs6=document.getElementById("gs6");
 var gs=document.getElementsByClassName("gs");
 
+var selected = [0,0,0,0,0,0];
+var loudestFreq;
+
 
 // each bin equal to 22050/16384 = 1.346 Hz
 
@@ -31,3 +34,109 @@ gs3.addEventListener("click", readstringinfo);
 gs4.addEventListener("click", readstringinfo);
 gs5.addEventListener("click", readstringinfo);
 gs6.addEventListener("click", readstringinfo);
+
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+function getLoudestFrequency(low,high) {
+    var halfsampleRate = sampleRate() / 2; // 22050
+    var spectrum = fft.analyze();               // array of amplitudes in bins
+    var bins = spectrum.length;                 // 16384 bins
+    var maxAmp = 0;
+    var largestBin;
+  // each bin equal to 22050/16384 = 1.346 Hz
+    for (var i = low; i < high; i++) {          //for loop to find the largest amplitude of bin
+        var thisAmp = spectrum[i];              // amplitude of current bin
+        if (thisAmp > maxAmp) {
+            maxAmp = thisAmp;
+            // if(maxAmp>100){                     //ignore the background sound effect
+              largestBin = i;                   //store the location of largestbin
+            // }
+        }
+    }
+    loudestFreq = largestBin * (halfsampleRate / bins);
+    // console.log(maxAmp);
+    return loudestFreq;
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+function indicator(){
+  if(getLoudestFrequency(selected[3],selected[4])<selected[2] && getLoudestFrequency(selected[3],selected[4])>selected[1]){
+    fill(3, 255, 1);
+    text('<',235,50);
+    text(selected[5],150,50);
+    text('>',65,50);
+  }else if (selected[5]!== 0 ) { //if the frequency is not correct.
+    fill(255);
+    text(selected[5],150,50);
+    if(getLoudestFrequency(selected[3],selected[4])>selected[2]){ //change color of the arror
+      fill(220,53,69);
+      text('<',235,50);
+      fill(255);
+      text('>',65,50);
+    }else if (getLoudestFrequency(selected[3],selected[4])<selected[1]) { //change color of the arror
+      fill(255);
+      text('<',235,50);
+      fill(220,53,69);
+      text('>',65,50);
+    }
+  }else{
+    fill(255);
+    text("Select a string",150,50);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+//Check user is clicking Guitar string or Ukulele string.
+////////////////////////////////////////////////////////////////////////////////////
+
+function readstringinfo(){
+  if(this.className === 'gs'){
+    if(this.id === freq_gs1[0]){
+      selected = freq_gs1;
+    }
+    else if (this.id === freq_gs2[0]){
+      selected = freq_gs2;
+    }
+    else if (this.id === freq_gs3[0]){
+      selected = freq_gs3;
+    }
+    else if (this.id === freq_gs4[0]){
+      selected = freq_gs4;
+    }
+    else if (this.id === freq_gs5[0]){
+      selected = freq_gs5;
+    }
+    else if (this.id === freq_gs6[0]){
+      selected = freq_gs6;
+    }
+  }else if (this.className ==='ukulele') {
+
+  }
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
+/// Change color when select the string
+////////////////////////////////////////////////////////////////////////////////////
+
+function selectstring(){
+  var counter = 0;
+  for (var i = 0; i < gs.length; i++) { //chech any string is selected or not.
+    if(gs[i].style.background === "blue"){
+      counter = counter +1;
+    }
+  }
+  if (counter == 6 ) {   // no strings have been selected
+    this.style.background = "red";
+  }else{   // some string have been selected
+    for (var i = 0; i < gs.length; i++) {
+      gs[i].style.background = "blue" //change all back to blue
+    }
+    this.style.background = "red";  //change the select one to red
+  }
+}
